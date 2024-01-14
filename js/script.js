05709e01ff2f566715fa587532734cd4195c773c
holl <lelongho998@gmail.com>
2024-01-14 16:40:33 +0700

    reverttttt

--- js/script.js
+++ js/script.js
@@ -389,7 +389,7 @@
         = COUNTDOWN CLOCK
     -------------------------------------------*/
     if ($("#clock").length) {
-        $('#clock').countdown('2024/03/09', function(event) {
+        $('#clock').countdown('2025/12/25', function(event) {
             var $this = $(this).html(event.strftime(''
             + '<div class="box"><div>%D</div> <span>Days</span> </div>'
             + '<div class="box"><div>%H</div> <span>Hours</span> </div>'
